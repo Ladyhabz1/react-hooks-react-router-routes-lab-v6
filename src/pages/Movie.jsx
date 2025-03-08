c91197faf758da8f8f0cb9ab49887d00f0003543
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-08 10:53:30 +0300

    Complete labs

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,31 @@
-import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((r) => r.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.error(error));
+  }, [id]);
+
+  if (!movie) return <h1>Loading...</h1>;
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>Time: {movie.time} minutes</p>
+      <p>Genres:</p>
+      {movie.genres.map((genre, index) => (
+        <span key={index}>{genre} </span>
+      ))}
     </>
   );
-};
+}
 
 export default Movie;
